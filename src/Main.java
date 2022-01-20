4806859a8567b0b3e93e0c58a7421df94c6f31a5
melody <melody.vikstrom@gmail.com>
2022-01-20 14:36:34 +0100

    New branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World again! and again. Changing this clone and Merge");
+        System.out.println("Hello World! This is now a new feature!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What's your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name + "!");
     }
 }
