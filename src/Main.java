0b7ada21fdcc1222881b5c4e88f42a92e92a9035
melody <melody.vikstrom@gmail.com>
2022-02-16 08:50:54 +0100

    added a hello

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World again! and again. Changing this clone.Merge");
+        System.out.println("Hello");
     }
 }
